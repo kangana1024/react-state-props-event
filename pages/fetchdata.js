d6577deb9a69119f5cc0667d9f3096e14d8bf66f
Kanchana Sondee <kangana512@gmail.com>
2021-12-21 11:58:14 +0700

    add loading

--- pages/fetchdata.js
+++ pages/fetchdata.js
@@ -2,22 +2,27 @@ import { Component } from "react";
 
 export default class App extends Component {
 	state = {
-		data: null
+		data: null,
+		loading: true
 	}
 	componentDidMount() {
+
 		fetch("https://randomuser.me/api/")
 			.then((response) => response.json())
 			.then((user) => {
 				this.setState({
-					data: user
+					data: user.results[0]
 				})
+				this.setState({ loading: false })
 			})
 	}
+
 	render() {
-		return (
+		return !this.loading ? (
 			<div>
 				{JSON.stringify(this.state.data)}
+				<p>Name :{this.state.data ? JSON.stringify(this.state.data.name) : null}</p>
 			</div>
-		)
+		) : <div>Loading</div>
 	}
 }
